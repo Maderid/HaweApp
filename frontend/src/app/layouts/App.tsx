8909ac919dcb076f457ca8a87eb1f0e7ca85bbd7
Maderide Meyer <MaderidM@agilebridge.co.za>
2026-02-10 20:14:15 +0200

    Made Initial folder structure and code requirements

---
+++ frontend/src/app/layouts/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Hawe App</h1>
+      <h4>wellness app</h4>
+    </div>
+  )
+}
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'lemon': '#C4E64C',
+        'orange': '#FFB84D',
+      },
+    },
+  },
+  plugins: [],
+}
