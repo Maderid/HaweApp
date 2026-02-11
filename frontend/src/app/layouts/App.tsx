ed1a11c509072af15b0370458947a45a7ab5a2cd
Maderide Meyer <MaderidM@agilebridge.co.za>
2026-02-11 20:48:23 +0200

    feat: add MorningCard component and UI card structure
    - Added MorningCard component to the app layout.
    - Introduced a new card UI component with Card, CardHeader, CardTitle, CardDescription, CardContent, and CardFooter.
    - Updated Tailwind CSS configuration to support custom colors and dark mode.
    - Added CSS variables for theming and improved styling with Tailwind CSS.
    - Included utility functions for class name management using clsx and tailwind-merge.
    - Updated package.json with new dependencies for styling and icons.

--- frontend/src/app/layouts/App.tsx
+++ frontend/src/app/layouts/App.tsx
@@ -1,8 +1,11 @@
+import MorningCard from "../../components/cards/MorningCard"
+
 export default function App() {
   return (
     <div className="flex items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Hawe App</h1>
       <h4>wellness app</h4>
+      <MorningCard/>
     </div>
   )
 }
